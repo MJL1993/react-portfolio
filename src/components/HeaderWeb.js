f81277dc2aa46240d7b2ad1d156a7096a6e27542
Marshall Lowry <67163811+MJL1993@users.noreply.github.com>
2021-01-11 19:08:57 -0600

    Update HeaderWeb.js

--- src/components/HeaderWeb.js
+++ src/components/HeaderWeb.js
@@ -23,14 +23,8 @@ export default function DenseAppBar() {
           <Typography variant="h5" color="inherit">
             <a className="HomePage" href="/">Home</a>
           </Typography>
-          <Typography className="LinkedIn" variant="h6" color="inherit">
-            <a  target="_blank" href="https://www.linkedin.com/in/zachary-yanez/">LinkedIn</a>
-          </Typography>
           <Typography className="Github" variant="h6" color="inherit">
-            <a target="_blank" href="https://github.com/ZachYanez">Github</a>
-          </Typography>
-          <Typography className="eResume" variant="h6" color="inherit">
-            <a target="_blank" href="https://drive.google.com/file/d/1UYyQI0CRbvNmUQnuC86onmz4mer0Jhsg/view">Entertainment Resume</a>
+            <a target="_blank" href="https://github.com/MJL1993">Github</a>
           </Typography>
         </Toolbar>
       </AppBar>
